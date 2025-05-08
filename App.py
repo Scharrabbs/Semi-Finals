a6372c80f44eb7257a1bd820c125b22763035ab2
Scharrabbs <sarabia.schlwyn@cec.edu.ph>
2025-05-08 09:49:41 +0800

    Third Update

--- App.py
+++ App.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return ('<h1>Schlwyn S. Sarabia</h1>'
-            '<h2>BSIT - 3A</h2>'
-            '<h3>System Integration and Architecture 1</h3>'
-            '<h4>Semi-Final Exam</h4>')
+            '<h1>BSIT - 3A</h1>'
+            '<h1>System Integration and Architecture 1</h1>'
+            '<h1>Semi-Final Exam</h1>')
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
