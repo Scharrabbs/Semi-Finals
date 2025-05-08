c4dcd0ca05c21f09a74c3ebdfaa05d4730205562
Scharrabbs <sarabia.schlwyn@cec.edu.ph>
2025-05-08 09:47:26 +0800

    Second Update

--- App.py
+++ App.py
@@ -1,7 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return ('<h1>Schlwyn S. Sarabia</h1>'
+            '<h2>BSIT - 3A</h2>'
+            '<h3>System Integration and Architecture 1</h3>'
+            '<h4>Semi-Final Exam</h4>')
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
